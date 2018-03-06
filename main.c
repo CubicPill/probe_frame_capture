9917ffba554963d81657238f3b603604fe2dbf54
CubicPill <huazhengchang97@gmail.com>
2018-03-06 23:02:08 +0800

    package

--- main.c
+++ main.c
@@ -11,25 +11,22 @@
 /* prototype of the packet handler */
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
 
-int main()
-{
+int main() {
     pcap_if_t *alldevs;
     pcap_if_t *d;
     int inum;
-    int i=0;
+    int i = 0;
     pcap_t *adhandle;
     char errbuf[PCAP_ERRBUF_SIZE];
 
     /* Retrieve the device list */
-    if(pcap_findalldevs(&alldevs, errbuf) == -1)
-    {
-        fprintf(stderr,"Error in pcap_findalldevs: %s\n", errbuf);
+    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+        fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
         exit(1);
     }
 
     /* Print the list */
-    for(d=alldevs; d; d=d->next)
-    {
+    for (d = alldevs; d; d = d->next) {
         printf("%d. %s", ++i, d->name);
         if (d->description)
             printf(" (%s)\n", d->description);
@@ -37,17 +34,15 @@ int main()
             printf(" (No description available)\n");
     }
 
-    if(i==0)
-    {
+    if (i == 0) {
         printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
         return -1;
     }
 
-    printf("Enter the interface number (1-%d):",i);
+    printf("Enter the interface number (1-%d):", i);
     scanf("%d", &inum);
 
-    if(inum < 1 || inum > i)
-    {
+    if (inum < 1 || inum > i) {
         printf("\nInterface number out of range.\n");
         /* Free the device list */
         pcap_freealldevs(alldevs);
@@ -55,19 +50,18 @@ int main()
     }
 
     /* Jump to the selected adapter */
-    for(d=alldevs, i=0; i< inum-1 ;d=d->next, i++);
+    for (d = alldevs, i = 0; i < inum - 1; d = d->next, i++);
 
     /* Open the device */
     /* Open the adapter */
-    if ((adhandle= pcap_open_live(d->name,	// name of the device
-                                  65536,			// portion of the packet to capture.
+    if ((adhandle = pcap_open_live(d->name,    // name of the device
+                                   65536,            // portion of the packet to capture.
             // 65536 grants that the whole packet will be captured on all the MACs.
-                                  1,				// promiscuous mode (nonzero means promiscuous)
-                                  1000,			// read timeout
-                                  errbuf			// error buffer
-    )) == NULL)
-    {
-        fprintf(stderr,"\nUnable to open the adapter. %s is not supported by WinPcap\n", d->name);
+                                   1,                // promiscuous mode (nonzero means promiscuous)
+                                   1000,            // read timeout
+                                   errbuf            // error buffer
+    )) == NULL) {
+        fprintf(stderr, "\nUnable to open the adapter. %s is not supported by WinPcap\n", d->name);
         /* Free the device list */
         pcap_freealldevs(alldevs);
         return -1;
@@ -87,8 +81,7 @@ int main()
 
 
 /* Callback function invoked by libpcap for every incoming packet */
-void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data)
-{
+void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data) {
     struct tm *ltime;
     char timestr[16];
     time_t local_tv_sec;
@@ -99,10 +92,22 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 
     /* convert the timestamp to readable format */
     local_tv_sec = header->ts.tv_sec;
-    ltime=localtime(&local_tv_sec);
-    strftime( timestr, sizeof timestr, "%H:%M:%S", ltime);
+    ltime = localtime(&local_tv_sec);
+    strftime(timestr, sizeof timestr, "%H:%M:%S", ltime);
 
     printf("%s,%.6d len:%d\n", timestr, header->ts.tv_usec, header->len);
-    printf("%s\n",param);
+    printf("##########\n");
+    for (int i = 0; i < header->len; ++i) {
+        printf("%02X ", pkt_data[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    if(header->len>12){
+        printf("Dst:\n");
+        printf("Src:\n");
+
+    }
+    printf("\n##########\n\n");
 
 }
