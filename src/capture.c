b34ae8c59ddefa907c55c598fb014b042e4cd133
CubicPill <huazhengchang97@gmail.com>
2018-03-14 16:01:05 +0800

    change args

--- src/capture.c
+++ src/capture.c
@@ -23,70 +23,41 @@ char whitelist_mac[18];
 
 int main(int argc, char *const argv[]) {
     memset(whitelist_mac, 0, 18);
-    if (argc < 3) {
-        fprintf(stderr, "No server address or port specified!");
+    if (argc < 4) {
+        fprintf(stderr, "No interface, server address or port specified!\n");
         exit(1);
     }
-    if (argc == 4) {
-        strncpy(whitelist_mac, argv[3], 18);
+    if (argc == 5) {
+        if (strlen(whitelist_mac) != 17) {
+            printf("Invalid MAC %s, whitelist will not be enabled\n", argv[4]);
+        } else {
+            strncpy(whitelist_mac, argv[4], 18);
+        }
     }
+    char *interface = argv[1];
+    char *ip = argv[2];
+    char *port = argv[3];
+
     sock = socket(AF_INET, SOCK_DGRAM, 0);
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons((unsigned short) strtol(argv[2], NULL, 10));
-    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
-        printf("inet_pton error for %s\n", argv[1]);
+    server_addr.sin_port = htons((unsigned short) strtol(port, NULL, 10));
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        printf("inet_pton error for %s\n", ip);
         exit(1);
     }
 
-
-    pcap_if_t *alldevs;
-    pcap_if_t *d;
-    int inum;
-    int i = 0;
     pcap_t *adhandle;
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
-        fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
-        exit(1);
-    }
 
-    for (d = alldevs; d; d = d->next) {
-        printf("%d. %s", ++i, d->name);
-        if (d->description)
-            printf(" (%s)\n", d->description);
-        else
-            printf(" (No description available)\n");
-    }
-
-    if (i == 0) {
-        printf("\nNo interfaces found! Make sure Libpcap / WinPcap is installed.\n");
+    if ((adhandle = pcap_open_live(interface, 65536, 1, 1000, errbuf)) == NULL) {
+        fprintf(stderr, "\nUnable to open the adapter %s \n", interface);
         return -1;
     }
 
-    printf("Enter the interface number (1-%d):", i);
-    scanf("%d", &inum);
+    printf("\nListening on %s...\n\n", interface);
 
-    if (inum < 1 || inum > i) {
-        printf("\nInterface number out of range.\n");
-        /* Free the device list */
-        pcap_freealldevs(alldevs);
-        return -1;
-    }
-
-    for (d = alldevs, i = 0; i < inum - 1; d = d->next, i++);
-
-
-    if ((adhandle = pcap_open_live(d->name, 65536, 1, 1000, errbuf)) == NULL) {
-        fprintf(stderr, "\nUnable to open the adapter. %s is not supported by Libpcap / WinPcap\n", d->name);
-        pcap_freealldevs(alldevs);
-        return -1;
-    }
-
-    printf("\nlistening on %s...\n", d->description);
-
-    pcap_freealldevs(alldevs);
     pcap_loop(adhandle, 0, packet_handler, NULL);
 
     pcap_close(adhandle);
